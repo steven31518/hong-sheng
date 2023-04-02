ba3e530e93a273db902f405a51fa38a7ee57297a
Steven Tsan <james31518@gmail.com>
2023-04-02 18:04:41 +0800

    coupon page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import DashBoard from "./pages/admin/DashBoard";
 import AdminProducts from "./pages/admin/AdminProducts";
+import AdminCoupons from "./pages/admin/AdminCoupons";
 function App() {
   return (
     <div className="App">
@@ -9,6 +10,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/admin" element={<DashBoard />}>
           <Route path="products" element={<AdminProducts />} />
+          <Route path="coupons" element={<AdminCoupons />} />
         </Route>
       </Routes>
     </div>
--- src/pages/admin/AdminCoupons.js
+++ src/pages/admin/AdminCoupons.js
@@ -1,55 +1,55 @@
 import axios from "axios";
 import { useEffect, useState, useRef } from "react";
-import ProductModal from "../../Components/ProductModal";
+import CouponModal from "../../Components/CouponModal";
 import DeleteModal from "../../Components/DeleteModal";
 import Pagination from "../../Components/Pagination";
 import { Modal } from "bootstrap";
 
-const AdminProducts = () => {
-  const [products, setProducts] = useState([]);
+const AdminCoupons = () => {
+  const [coupons, setCoupons] = useState([]);
   const [pagination, setPagination] = useState({});
   //type:決定Modal的用途
   const [type, setType] = useState("create"); //edit
-  const [tempProduct, setTempProduct] = useState({});
-  const productModal = useRef(null);
+  const [tempCoupon, setTempCoupon] = useState({});
+  const couponModal = useRef(null);
   const deleteModal = useRef(null);
   useEffect(() => {
-    productModal.current = new Modal("#productModal", { backdrop: "static" });
+    couponModal.current = new Modal("#productModal", { backdrop: "static" });
     deleteModal.current = new Modal("#deleteModal", { backdrop: "static" });
 
-    getProducts();
+    getCoupons();
   }, []);
-  const getProducts = async (page = 1) => {
-    const productRes = await axios.get(
-      `/v2/api/${process.env.REACT_APP_API_PATH}/admin/products?page=${page}`
+  const getCoupons = async (page = 1) => {
+    const couponRes = await axios.get(
+      `/v2/api/${process.env.REACT_APP_API_PATH}/admin/coupons?page=${page}`
     );
-    console.log(productRes);
-    setProducts(productRes.data.products);
-    setPagination(productRes.data.pagination);
+    console.log(couponRes);
+    setCoupons(couponRes.data.coupons);
+    setPagination(couponRes.data.pagination);
   };
 
-  const openProductModal = (type, product) => {
+  const openCouponModal = (type, item) => {
     setType(type);
-    setTempProduct(product);
-    productModal.current.show();
+    setTempCoupon(item);
+    couponModal.current.show();
   };
-  const closeProductModal = () => {
-    productModal.current.hide();
+  const closeModal = () => {
+    couponModal.current.hide();
   };
-  const openDeleteModal = (product) => {
-    setTempProduct(product);
+  const openDeleteModal = (item) => {
+    setTempCoupon(item);
     deleteModal.current.show();
   };
   const closeDeleteModal = () => {
     deleteModal.current.hide();
   };
-  const deleteProduct = async (id) => {
+  const deleteCoupon = async (id) => {
     try {
       const res = await axios.delete(
-        `/v2/api/${process.env.REACT_APP_API_PATH}/admin/product/${id}`
+        `/v2/api/${process.env.REACT_APP_API_PATH}/admin/coupon/${id}`
       );
       if (res.data.success) {
-        getProducts();
+        getCoupons();
         deleteModal.current.hide();
       }
     } catch (error) {
@@ -59,52 +59,54 @@ const AdminProducts = () => {
   return (
     <>
       <div className="p-3">
-        <ProductModal
-          closeProductModal={closeProductModal}
-          getProducts={getProducts}
+        <CouponModal
+          closeModal={closeModal}
+          getCoupons={getCoupons}
           type={type}
-          tempProduct={tempProduct}
+          tempCoupon={tempCoupon}
         />
         <DeleteModal
           closeDeleteModal={closeDeleteModal}
-          text={tempProduct.title}
-          handleDelete={deleteProduct}
-          id={tempProduct.id}
+          text={tempCoupon.title}
+          handleDelete={deleteCoupon}
+          id={tempCoupon.id}
         />
-        <h3>產品列表</h3>
+        <h3>優惠卷列表</h3>
         <hr />
         <div className="text-end">
           <button
             type="button"
             className="btn btn-primary btn-sm"
-            onClick={() => openProductModal("create", {})}
+            onClick={() => openCouponModal("create", {})}
           >
-            建立新商品
+            建立新優惠卷
           </button>
         </div>
         <table className="table">
           <thead>
             <tr>
-              <th scope="col">分類</th>
-              <th scope="col">名稱</th>
-              <th scope="col">售價</th>
+              <th scope="col">標題</th>
+              <th scope="col">折扣</th>
+              <th scope="col">到期日</th>
+              <th scope="col">優惠碼</th>
               <th scope="col">啟用狀態</th>
               <th scope="col">編輯</th>
             </tr>
           </thead>
           <tbody>
-            {products.map((product) => {
+            {coupons.map((product) => {
               return (
                 <tr key={product.id}>
-                  <td>{product.category}</td>
                   <td>{product.title}</td>
-                  <td>{product.price}</td>
+                  <td>{product.percent}</td>
+                  <td>{product.due_date}</td>
+                  <td>{product.code}</td>
                   <td>{product.is_enabled ? "啟用" : "未啟用"}</td>
                   <td>
                     <button
                       type="button"
                       className="btn btn-primary btn-sm"
-                      onClick={() => openProductModal("edit", product)}
+                      onClick={() => openCouponModal("edit", product)}
                     >
                       編輯
                     </button>
@@ -121,9 +123,9 @@ const AdminProducts = () => {
             })}
           </tbody>
         </table>
-        <Pagination pagination={pagination} changePage={getProducts} />
+        <Pagination pagination={pagination} changePage={getCoupons} />
       </div>
     </>
   );
 };
-export default AdminProducts;
+export default AdminCoupons;
